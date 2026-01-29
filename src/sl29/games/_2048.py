31edb86fccae2a7e7373099fd1a93e697af498f8
aemu-lus <philippe-e@saint-louis29.net>
2026-01-29 17:09:35 +0100

    Fonction ajouter_tuile et supprimer_zero

--- src/sl29/games/_2048.py
+++ src/sl29/games/_2048.py
@@ -2,6 +2,7 @@
 
 import random
 from typing import List, Tuple
+import copy
 
 TAILLE:int = 4
 
@@ -17,7 +18,10 @@ def nouvelle_partie() -> Tuple[List[List[int]], int]:
     :return: Une grille TAILLExTAILLE initialisée avec deux tuiles, ainsi que le score à 0.
     :rtype: Tuple[List[List[int]], int]
     """
-    raise NotImplementedError("Fonction nouvelle_partie non implémentée.")
+    grille = _creer_plateau_vide()
+    grille2 = _ajouter_tuile(grille)
+    grille3 = _ajouter_tuile(grille2)
+    return (grille3, 0)
 
 def jouer_coup(plateau: List[List[int]], direction: str) -> tuple[List[List[int]], int, bool]:
     """
@@ -78,7 +82,14 @@ def _ajouter_tuile(plateau: List[List[int]]) -> List[List[int]]:
     :return: Une nouvelle grille avec une tuile ajoutée.
     :rtype: List[List[int]]
     """
-    raise NotImplementedError("Fonction _ajouter_tuile non implémentée.")
+    plateau = copy.deepcopy(plateau)
+    cv = _get_cases_vides(plateau)
+    pos = random.randint(0, len(cv)-1)
+    coord = cv[pos]
+    ligne = coord[0]
+    colone = coord[1]
+    plateau[ligne][colone] = 2
+    return plateau
 
 def _supprimer_zeros(ligne: List[int]) -> List[int]:
     """
@@ -89,7 +100,7 @@ def _supprimer_zeros(ligne: List[int]) -> List[int]:
     :return: La ligne sans zéros.
     :rtype: List[int]
     """
-    raise NotImplementedError("Fonction _supprimer_zeros non implémentée.")
+   
 
 def _fusionner(ligne: List[int]) -> Tuple[List[int], int]:
     """
